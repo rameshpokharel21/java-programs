958fd1af33b74681aceede063317288149b84682
rameshpokharel21 <pokharelramesh@yahoo.com>
2022-12-05 18:24:05 -0500

    first java code added.

---
+++ ReverseWord.java
@@ -0,0 +1,25 @@
+/*
+ * A simple java program to reverse a given string.
+ */
+
+ package java_programs;
+
+ import java.util.Scanner;
+
+ public class ReverseWord{
+
+ 	public static void main(String[] args){
+
+ 		Scanner scanner = new Scanner(System.in);
+
+ 		System.out.println("Enter a word to reverse:");
+
+ 		String word = scanner.nextLine();
+        String reverse = new StringBuilder(word).reverse().toString();
+ 		System.out.println("The reverse of \'%s\' is \'%s\'.%n".formatted(word, reverse));
+
+ 		scanner.close();
+ 	}
+
+ 	
+ }
